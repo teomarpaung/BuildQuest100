4d8df152a415deed26290365843e267b28bea191
teomarpaung <teomarpaung@gmail.com>
2026-03-31 00:39:08 +0900

    Delete harian.py

--- harian.py
+++
@@ -1,7 +0,0 @@
-pengeluaran = int(input("Masukkan total pengeluaran hari ini (Rp): "))
-target = 1000000 # batas harian
-
-if pengeluaran > target:
-    print("Kamu melebihi batas harian! Kurangi pengeluaran besok.")
-else:
-    print("Bagus! Kamu masih dalam batas wajar.")
