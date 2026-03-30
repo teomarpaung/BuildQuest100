ffc1525b4067ff85fb7bc243da8a0f6685020478
teomarpaung <teomarpaung@gmail.com>
2026-03-31 01:06:57 +0900

    Create day6_counter.py

---
+++ phase1_basic/day6_counter.py
@@ -0,0 +1,12 @@
+angka = 0
+while angka < 5:
+    print("Angka: ", angka)
+    angka += 1
+
+counter = 0
+nama = ["Andi", "Budi", "Citra", "Danang", "Eka"]
+while counter < len(nama):   
+    print("Counter: ", counter)
+    print("Selamat belajar, ", nama[counter])
+    counter += 1
+
